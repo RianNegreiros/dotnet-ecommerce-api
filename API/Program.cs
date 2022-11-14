0e1702f06957330702202b64c315ab967d195f54
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-11-14 17:04:47 -0300

    refactor project folder organization

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,4 @@
-using API.Data;
+using Infra.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
--- Infra/Data/Migrations/20221114192145_InitialCreate.cs
+++ Infra/Data/Migrations/20221114192145_InitialCreate.cs
@@ -2,7 +2,7 @@
 
 #nullable disable
 
-namespace API.Data.Migrations
+namespace Infra.Data.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
