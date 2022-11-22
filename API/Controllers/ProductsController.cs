5a710d9ada5f88e1ea48dccd0f858d06e2c4a5d5
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-11-22 12:33:03 -0300

    cache products

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -27,6 +27,7 @@ namespace API.Controllers
             _mapper = mapper;
         }
 
+        [Cached(600)]
         [HttpGet]
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productParams)
         {
@@ -42,7 +43,8 @@ namespace API.Controllers
 
             return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
         }
-        
+
+        [Cached(600)]
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
@@ -54,12 +56,14 @@ namespace API.Controllers
             return Ok(_mapper.Map<Product, ProductToReturnDto>(product));
         }
 
+        [Cached(600)]
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
             return Ok(await _productBrandsRepo.ListAllAsync());
         }
 
+        [Cached(600)]
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
